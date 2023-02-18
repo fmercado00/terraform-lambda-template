33463fa8db34d2573bc99c6616f0143066eb3063
Francisco Mercado <fmercado00@hotmail.com>
2023-02-18 12:32:43 -0600

    chore: add rule test

--- main.tf
+++ main.tf
@@ -33,5 +33,24 @@ resource "aws_lambda_function" "simplification_lambda_function" {
     environment = var.environment
     project     = var.project
   }
+}
 
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "simplification_lambda"
+    arn = aws_lambda_function.simplification_lambda_function.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+    statement_id = "AllowExecutionFromCloudWatch_simplification_lambda"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.simplification_lambda_function.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
 }
